7eb2e504881c468ee6df19abedc854ce53292de7
Ritik <160255700+gritik418@users.noreply.github.com>
2026-02-09 19:42:54 +0530

    feat: register commentApi in redux store

--- store/index.ts
+++ store/index.ts
@@ -5,6 +5,7 @@ import workspacesSlice from "@/features/workspace/workspace.slice";
 import projectApi from "@/features/project/project.api";
 import projectsSlice from "@/features/project/project.slice";
 import taskApi from "@/features/task/task.api";
+import commentApi from "@/features/comment/comment.api";
 import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
@@ -16,13 +17,15 @@ const store = configureStore({
     [workspaceApi.reducerPath]: workspaceApi.reducer,
     [projectApi.reducerPath]: projectApi.reducer,
     [taskApi.reducerPath]: taskApi.reducer,
+    [commentApi.reducerPath]: commentApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(organizationApi.middleware)
       .concat(workspaceApi.middleware)
       .concat(projectApi.middleware)
-      .concat(taskApi.middleware),
+      .concat(taskApi.middleware)
+      .concat(commentApi.middleware),
 });
 
 export default store;
