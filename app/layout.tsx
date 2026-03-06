924aaabff5519f3dd0ade2877df2b01fa936fbac
Ritik <160255700+gritik418@users.noreply.github.com>
2026-03-06 17:49:52 +0530

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "react-hot-toast";
 import QueryClientProviderComponent from "@/providers/QueryClientProviderComponent";
 import { AuthProvider } from "@/providers/AuthProvider";
 import ReduxProvider from "@/providers/ReduxProvider";
+import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,9 +21,9 @@ const jetbrainsMono = JetBrains_Mono({
 
 export const metadata: Metadata = {
   title: "Trackr | High-velocity Project Management",
-  description: "Manage projects with precision. The new standard for high-performance teams.",
+  description:
+    "Manage projects with precision. The new standard for high-performance teams.",
 };
-
 
 export default function RootLayout({
   children,
@@ -31,16 +32,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}>       
-    <QueryClientProviderComponent>
-      <ReduxProvider>
-
-<AuthProvider>
-        {children}
-</AuthProvider>
-      </ReduxProvider>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <body
+        className={`${inter.variable} ${jetbrainsMono.variable} font-sans antialiased`}
+      >
+        <QueryClientProviderComponent>
+          <ReduxProvider>
+            <AuthProvider>{children}</AuthProvider>
+          </ReduxProvider>
           <Toaster />
-    </QueryClientProviderComponent>
+        </QueryClientProviderComponent>
       </body>
     </html>
   );
